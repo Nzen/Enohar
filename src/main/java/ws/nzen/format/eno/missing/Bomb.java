ced455958e2b584f32cec1975081f2024591f5ef
Nicholas Prado <nmprado@nzen.ws>
2019-04-15 22:46:31 -0500

    Bomb and FakeField override more methods with no ops

--- src/main/java/ws/nzen/format/eno/missing/Bomb.java
+++ src/main/java/ws/nzen/format/eno/missing/Bomb.java
@@ -1,5 +1,8 @@
 package ws.nzen.format.eno.missing;
 
+import java.util.LinkedList;
+import java.util.List;
+import java.util.NoSuchElementException;
 
 /** to ensure all fake elements can complain */
 public interface Bomb
@@ -8,6 +11,113 @@ public interface Bomb
 	public String getComplaint();
 
 	public void setComplaint( String why );
+
+
+	/** Congrats, you just cut the safe wire. */
+	public default String stringKey()
+	{
+		return getName();
+	}
+	/** Congrats, you just cut the safe wire. */
+	public default String getName()
+	{
+		return null;
+	}
+	/** no op */
+	public default void setName( String name )
+	{
+	}
+
+
+	public default int getNameEscapes()
+	{
+		return 0; // snide is -1
+	}
+	public default void setNameEscapes( int nameEscapes )
+	{
+	}
+
+
+	/** no op */
+	public default void addComment( String another )
+	{
+	}
+	/** Congrats, you just cut the wrong wire.
+	 *  @throws NoSuchElementException inflexibly */
+	public default String requiredStringComment()
+	{
+		throw new NoSuchElementException( getComplaint() );
+	}
+	/** Congrats, you just cut the safe wire. */
+	public default String optionalStringComment()
+	{
+		return null;
+	}
+
+	/** Congrats, you just cut the safe wire. */
+	public default List<String> getComments()
+	{
+		return new LinkedList<>();
+	}
+	/** no op */
+	public default void setComments( List<String> comments )
+	{
+	}
+	/** no op */
+	public default void cloneComments( List<String> comments )
+	{
+	}
+
+
+	public default boolean firstCommentPreceededName()
+	{
+		return false;
+	}
+	public default void setFirstCommentPreceededName( boolean firstCommentPreceededName )
+	{
+	}
+
+
+	public default int getPreceedingEmptyLines()
+	{
+		return 0;
+	}
+	public default void setPreceedingEmptyLines( int preceedingEmptyLines )
+	{
+	}
+
+	public default String getTemplateName()
+	{
+		return null;
+	}
+	public default void setTemplateName( String templateElementName )
+	{
+	}
+
+	public default int getTemplateEscapes()
+	{
+		return 0;
+	}
+	public default void setTemplateEscapes( int templateEscapes )
+	{
+	}
+
+	public default boolean isShallowTemplate()
+	{
+		return false;
+	}
+	public default void setShallowTemplate( boolean shallowTemplate )
+	{
+	}
+
+	public default int getLine()
+	{
+		return 0;
+	}
+	public default void setLine( int line )
+	{
+	}
+
 
 }
 
--- src/main/java/ws/nzen/format/eno/missing/FakeField.java
+++ src/main/java/ws/nzen/format/eno/missing/FakeField.java
@@ -1,6 +1,8 @@
 /** see ../../../../../LICENSE for release details */
 package ws.nzen.format.eno.missing;
 
+import java.util.NoSuchElementException;
+
 import ws.nzen.format.eno.EnoType;
 import ws.nzen.format.eno.Field;
 
@@ -35,6 +37,40 @@ public class FakeField extends Field implements Bomb
 			trigger = why;
 		else
 			trigger = "";
+	}
+
+
+	/** no op */
+	public void append( String more )
+	{
+	}
+
+
+	/** returns null */
+	public String optionalStringValue()
+	{
+		return null;
+	}
+
+
+	/** Congrats, you just cut the wrong wire.
+	 *  @throws NoSuchElementException inflexibly */
+	public String requiredStringValue()
+	{
+			throw new NoSuchElementException( trigger );
+	}
+
+
+	/** no op */
+	public void setStringValue( String newValue )
+	{
+	}
+
+
+	/** nope; null */
+	public boolean isEmpty()
+	{
+		return false;
 	}
 
 }
